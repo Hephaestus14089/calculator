e8d445e547acaac9d6d910d5c957406408d17057
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-10-22 16:39:32 +0530

    create src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::env::args;
+
+fn main() {
+
+    let mut args = args();
+    // println!("{:?}", args);
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number: f32 = second.parse().unwrap();
+    let result = operate(operator, first_number, second_number);
+    
+    println!("{:?}", output(first_number, second_number, operator, result));
+} // end of main()
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        '/' => first_number / second_number,
+        _ => panic!("Invalid operator")
+    }
+} // end of operate function
+
+fn output(first_number: f32, second_number: f32, operator: char, result: f32) -> String {
+
+    return format!("{} {} {} = {}", first_number, operator, second_number, result);
+} // end of output function
